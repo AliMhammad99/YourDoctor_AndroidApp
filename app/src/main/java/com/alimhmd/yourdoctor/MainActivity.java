b4e8a41cba0e83402de2210614f6e13acbb47c96
Ali Mhammad <alimhmd.engineer@gmail.com>
2022-04-06 08:38:22 +0300

    Initial commit

---
+++ app/src/main/java/com/alimhmd/yourdoctor/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alimhmd.yourdoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
